9d8fdac02db9e885cc0209ecd5d49042cadee3fb
Dmitrij Repnikov <dvrepa.develop@gmail.com>
2023-04-21 18:53:35 +0300

    Posts: Create and get posts

--- posts/index.js
+++ posts/index.js
@@ -1,1 +1,29 @@
-console.log(fetch)
+const express = require("express");
+const cors = require("cors");
+const { randomBytes } = require("crypto");
+const posts = require("./posts");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/posts", (_, res) => {
+  res.json(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).json(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on 4000");
+});

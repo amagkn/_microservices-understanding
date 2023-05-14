79ad12fbc2cbba707f24af4c506d9c2fef6a851c
Dmitrii Repnikov <dmitrii.amagkn@gmail.com>
2023-05-14 11:45:33 +0300

    Query service data receiving

--- query/index.js
+++ query/index.js
@@ -6,16 +6,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/events", (req, res) => {
-  const event = req.body;
+const posts = {};
 
-  console.log("Query service: received event", event.type);
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    posts[postId].comments.push({ id, content });
+  }
 
   res.sendStatus(200);
 });
 
 app.post("/posts", (req, res) => {
-  res.send({});
+  res.send(posts);
 });
 
 app.listen(4002, () => {
